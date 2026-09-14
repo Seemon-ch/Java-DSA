fa96d44191c7c389aea88aa8bf2e31bee5d9ef81
Seemon-ch <seemonchaudhary99@gmail.com>
2026-09-15 05:19:36 +0530

    pattern question

--- patterns/Pattern5.java
+++ patterns/Pattern5.java
@@ -18,12 +18,43 @@ public class Pattern5{
             System.out.println();
         }
     }
+
+    //solid rhombus 
+    public void pattern5b(int n){
+        for(int i=0 ;i<=n ;i++){
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    //hollow rhombus
+    public void pattern5c(int n){
+        for(int i=1 ;i<=n ;i++){
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1 ||i==n ||j==1 ||j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String arg[]){
         try(Scanner sc = new Scanner(System.in)){
             System.out.println("enter total number of rows");
             int t_r = sc.nextInt();
             Pattern5 obj = new Pattern5();
-            obj.pattern5a(t_r);
+            obj.pattern5c(t_r);
 
         }
 
